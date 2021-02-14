4be705dc6c6445c2c3dc6b57fa574e199745bf87
gjwehnes <gjwehnes@E829WL170006.org.cbe.ab.ca>
2021-02-14 16:51:14 -0700

    added ex015

---
+++ src/ex015/Divider.java
@@ -0,0 +1,13 @@
+package ex015;
+
+// Create a method that will take two integers and perform floating-point division.
+// The return value will be a double
+		
+		
+public class Divider {
+	
+	public static double divide(int a, int b) {
+		return (double)a / b;
+	}	
+
+}
---
+++ src/ex015/DividerTest.java
@@ -0,0 +1,16 @@
+package ex015;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DividerTest {
+
+	@Test
+	public void test1() {
+		
+		assertEquals(2, Divider.divide(4, 2), 0.01);		
+		assertEquals(2.5, Divider.divide(5, 2), 0.01);
+		
+	}
+
+}
