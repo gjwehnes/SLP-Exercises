77d2193f8c2cd7f00e41767a4befc95f4311782d
gjwehnes <gjwehnes@E829WL170006.org.cbe.ab.ca>
2021-02-14 17:50:00 -0700

    changed run.bash

--- run.bash
+++ run.bash
@@ -1,10 +1,6 @@
 echo "compile exercises"
 javac -classpath .:/run_dir/junit-4.12.jar:/run_dir/hamcrest-core-1.3.jar:/run_dir/json-simple-1.1.1.jar -d . src/ex015/*.java Main.java  
 echo "compile Main"
-<<<<<<< Upstream, based on origin/main
-javac -classpath .:/run_dir/junit-4.12.jar:/run_dir/hamcrest-core-1.3.jar:/run_dir/json-simple-1.1.1.jar -d Main.java  
-=======
 javac -classpath .:/run_dir/junit-4.12.jar:/run_dir/hamcrest-core-1.3.jar:/run_dir/json-simple-1.1.1.jar -d . Main.java  
->>>>>>> 3a9a73c updated test inventory
 echo "run Main.java"
 java -classpath .:/run_dir/junit-4.12.jar:/run_dir/hamcrest-core-1.3.jar:/run_dir/json-simple-1.1.1.jar Main
