c94e17cfda5ec67a39eff2191a65c84c3a3f90b5
gjwehnes <gjwehnes@shaw.ca>
2021-02-16 18:24:27 -0700

    Update run.bash

--- run.bash
+++ run.bash
@@ -3,4 +3,4 @@ javac -classpath .:/run_dir/junit-4.12.jar:/run_dir/hamcrest-core-1.3.jar:/run_d
 echo "compile Main"
 javac -classpath .:/run_dir/junit-4.12.jar:/run_dir/hamcrest-core-1.3.jar:/run_dir/json-simple-1.1.1.jar -d . Main.java  
 echo "run Main.java"
-java -classpath .:/run_dir/junit-4.12.jar:/run_dir/hamcrest-core-1.3.jar:/run_dir/json-simple-1.1.1.jar Main
+java -classpath .:/run_dir/junit-4.12.jar:/run_dir/hamcrest-core-1.3.jar:/run_dir/json-simple-1.1.1.jar:./bin Main
