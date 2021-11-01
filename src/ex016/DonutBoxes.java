ae4633483f455549e293958b8d15e200461e3b62
gjwehnes <gjwehnes@shaw.ca>
2021-11-01 09:43:23 -0600

    Update DonutBoxes.java

--- src/ex016/DonutBoxes.java
+++ src/ex016/DonutBoxes.java
@@ -1,8 +1,10 @@
 package ex016;
 /*
- * Create two methods that help a bakery calculate how many boxes will be required
+ * Create a method that calculates how many boxes a bakery will require
  * for an order of donuts. Each box can contain up to 12 donuts. Each order will require at
  * minimum one box, and obviously the bakery wants to use the minimum amount of boxes.
+ *
+ * Hint: this can be done using basic mathematical operators. No need for conditional statements!
 */
 public class DonutBoxes {
 	
