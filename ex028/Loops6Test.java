404ed2674afc945c6c96df53d1b9e12e9759177c
gjwehnes <gjwehnes@E829WL240006.org.cbe.ab.ca>
2025-02-05 19:55:10 -0700

    ex028 updated

--- ex028/Loops6Test.java
+++ ex028/Loops6Test.java
@@ -10,9 +10,57 @@ public class Loops6Test {
 		assertEquals(70, Loops6.investmentCalculator(1, 1, 2));
 		assertEquals(13, Loops6.investmentCalculator(11.11, 5.5, 22.22));
 		assertEquals(0, Loops6.investmentCalculator(1000, 5, 1000));
-		assertEquals(0, Loops6.investmentCalculator(1000, 5, 500));
-		assertEquals(-1, Loops6.investmentCalculator(1000, 0, 2000));
-		assertEquals(-1, Loops6.investmentCalculator(0, 5, 2000));
+		assertEquals(0, Loops6.investmentCalculator(1000, 5, 500));		
 	}
 
+    @Test
+    public void testInvalidInputs() {
+
+    	boolean arithmeticExceptionThrown = false;
+    	try {
+    		Loops6.investmentCalculator(1000, 0, 2000);
+            fail("Expected an ArithmeticException to be thrown");
+        } catch (ArithmeticException e) {
+        	arithmeticExceptionThrown = true;
+        }
+        assertEquals(true, arithmeticExceptionThrown);
+        
+        arithmeticExceptionThrown = false;
+        try {
+        	Loops6.investmentCalculator(0, 5, 2000);
+        	fail("Expected an ArithmeticException to be thrown");
+        } catch (ArithmeticException e) {
+        	arithmeticExceptionThrown = true;
+        }
+        
+        boolean illegalArgumentExceptionThrown = false;
+        try {
+        	Loops6.investmentCalculator(-1000, 5, 2000);
+        	fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+        	illegalArgumentExceptionThrown = true;
+        }
+        assertEquals(true, illegalArgumentExceptionThrown);
+
+        
+        illegalArgumentExceptionThrown = false;
+        try {
+        	Loops6.investmentCalculator(1000, -5, 2000);
+        	fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+        	illegalArgumentExceptionThrown = true;
+        }
+        assertEquals(true, illegalArgumentExceptionThrown);
+        
+        illegalArgumentExceptionThrown = false;
+        try {
+        	Loops6.investmentCalculator(1000, 5, -2000);
+        	fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+        	illegalArgumentExceptionThrown = true;
+        }
+        assertEquals(true, illegalArgumentExceptionThrown);
+        
+        
+    }
 }
--- ex028/instructions.md
+++ ex028/instructions.md
@@ -1,5 +1,16 @@
+### Loops6
+
 Write a method investmentCalculator that checks how many years it would take for an initial investment to reach a target investment, given compound growth at an annual interest rate.
  
-**Note**: The core of this calculation is simple, and can be written with a while loop. However, there are two unusual cases which would give you an infinite loop, because the initial investment would never reach the target investment. Mathematically, you would say that the answer is undefined. You will need to handle these separately. Coders have traditionally used a return value of -1 to indicate that the answer is undefined,
+#### Input
+- None of the parameters can be negative (to keep this problem realistic); if any  one is, throw an IllegalArgumentException
+- There are again unusual cases which would give you an infinite loop, because the initial investment would never reach the target investment. Mathematically, you would say that the answer is undefined. If this occurs, throw an ArithmeticException
+
+#### Output
+- See the unit tests for expected output; you can also verify this mathematically.
+
+#### Note
+- The core of this calculation is simple, and can be written with a while loop. 
+- There is one other edge case that you will need to be aware of!
 
 See the unit tests for example output.
